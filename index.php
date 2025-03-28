0ed6bfb087439a5855bafe415f97ea1e9ca69ab1
Meng Ronal <mengronal@gmail.com>
2025-03-28 11:11:29 +0700

    add new cv

---
+++ index.php
@@ -0,0 +1,80 @@
+<?php
+include './include/header.php';
+?>
+</head>
+
+<body>
+    <div class="container">
+        <div class="content_left">
+            <div class="content">
+                <div class="image">
+                    <img src="./image/1.jpg" alt="">
+                </div>
+                <div class="about">
+                    <h2> <span><i class="bi bi-person-fill"></i></span> About Me</h2>
+                    <p>I'm a very friendly, flexible honest, reliable, self-motivated, and hard working person. I am confident that my qualifications and skills could make a significant contribution the company. i'm a person who can work well as part of a team and on my own.</p>
+                </div>
+
+                <div class="Contact">
+                    <h2> <span><i class="bi bi-person-video2"></i></span> Contact</h2>
+                    <p><span><i class="bi bi-telephone"></i></span>089500112</p>
+                    <p><span><i class="bi bi-envelope-at"></i></span>mengronal@gmail.com</p>
+                    <p><span><i class="bi bi-geo-alt"></i></span>Kampong Svay commune, Serey Sophorn​ City, ​Banteay Meanchey province,</p>
+                </div>
+                <div class="Skill">
+                    <h2> <span><i class="bi bi-gear"></i></span> Skill</h2>
+                    <ul>
+                        <li>Microsoft Office</li>
+                        <li>C/C++</li>
+                        <li>HTML</li>
+                        <li>CSS</li>
+                        <li>Java Script</li>
+                        <li>React js</li>
+                        <li>PHP</li>
+                    </ul>
+                </div>
+                <div class="Language">
+                    <h2> <span><i class="bi bi-translate"></i></span> Language</h2>
+                    <ul>
+                        <li>Khmer : Native</li>
+                        <li>English : Medium</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <div class="content_right">
+            <div class="content">
+                <div class="name">
+                    <h1>Meng Ronal</h1>
+                    <h2>Apply For:</h2>
+                    <p>Web Developer</p>
+                </div>
+                <div class="Education">
+                    <h1><span><i class="bi bi-mortarboard-fill"></i></span>Education</h1>
+                    <h2>Present : Studying at Build Bright University</h2>
+                    <p>Bachelor degree of Information Teachnology</p>
+                </div>
+
+                <div class="hight_School">
+                    <h3>(2020 - 2023)</h3>
+                    <p>Samdach Ov High School</p>
+                    <h3>(2017 - 2020)</h3>
+                    <p>Kampong Svay Sccondary School</p>
+                </div>
+                <div class="Exsperience">
+                    <h1><span><i class="bi bi-briefcase-fill"></i></span>Exsperience</h1>
+                    <div class="First">
+                        <h2>First Company</h2>
+                        <h3>Sale Lead: Wing Bank (3Months)</h3>
+                        <ul>
+                            <li>Complete a lot of complicated word</li>
+                        </ul>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
